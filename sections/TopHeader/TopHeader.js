dda0e2bbbaef838a1cab3719a2f9f39f19df4ebf
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2023-04-13 21:52:25 +0600

    top header updated

--- sections/TopHeader/TopHeader.js
+++ sections/TopHeader/TopHeader.js
@@ -2,7 +2,7 @@ import Typewriter from 'typewriter-effect';
 
 const TopHeader = () => {
     return (
-        <div className="px-4 bg-gradient-to-r from-gray-100 via-pink-200 to-gray-100 py-16 mx-auto md:max-w-full max-w-[1800px] md:px-24 lg:px-8 lg:py-20">
+        <div className="px-4 bg-gradient-to-r from-gray-200 via-pink-200 to-gray-200 py-16 mx-auto md:max-w-full max-w-[1800px] md:px-24 lg:px-8 lg:py-20">
             <div className="grid gap-10 lg:grid-cols-2">
                 <div className="flex flex-col justify-center md:pr-8 xl:pr-0 lg:max-w-lg">
                     <div className="flex items-center justify-center w-16 h-16 mb-4 rounded-full bg-teal-accent-400">
